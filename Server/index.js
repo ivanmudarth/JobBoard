431289580636ba10514b154e6e74ebd8a8f9ced7
Dhruv Shah <dhruvshah@Dhruvs-MacBook-Air.local>
2023-03-06 21:00:53 -0500

    add api for getting jobs with filter

---
+++ Server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const db = require('./config/db')
+const port = 3000 
+
+
+app.use(express.json())
+
+
+// Route
+app.get("/api/getJobs/:title/:city/:state/:min_salary/:max_salary/:rating", (req,res)=>{
+  const title = req.params.title
+  const city = req.params.city
+  const state = req.params.state
+  const min_salary = req.params.min_salary
+  const max_salary = req.params.max_salary
+  const rating = req.params.rating
+
+
+  const isTitle = title !== "null" 
+  const isLocation = city !== "null" & state !== "null"
+  isSalary = min_salary !== "null" & max_salary !== "null"
+  isRating = rating !== "null" 
+
+  db.query(
+    "SELECT * \
+    FROM JobPosting \
+    WHERE IF (${isTitle}, UPPER(title) LIKE UPPER('%${title}%'), True) AND \
+        IF (${isLocation}, UPPER(city) = UPPER(${city}) AND \
+                  UPPER(state) = UPPER(${state}), True) AND \
+        IF (${isSalary}, avg_salary BETWEEN ${min_salary} AND ${max_salary}, True) AND \
+        IF (${isRating}, rating >= ${rating}, True)" , (err, result)=>{
+          if(err) {
+            console.log(err)
+          } 
+          res.send(result)
+        }
+  );
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
